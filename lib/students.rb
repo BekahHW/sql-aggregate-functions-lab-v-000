cb06e8d1423ffdd77d49dbe38e192ad59acf03c0
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-08-28 10:22:25 +0000

    automatically backed up by learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,9 +1,8 @@
 ## Code your solution below. Note that your SQL queries should be in quotation marks. 
 
 def highest_student_gpa
-SELECT MAX(gpa)
-FROM students;
-# WHERE condition;
+"#{SELECT MAX(gpa)
+FROM students;}"
 end
 
 def lowest_student_gpa
